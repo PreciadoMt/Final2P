b5196cbdd1c7f8ece4c48c03c5f9c1e09fb6bc0f
PreciadoMt <urielpreciado369@gmail.com>
2025-04-12 21:16:17 -0600

    aaaa

--- EntornoPeliculas/modelsPydantic.py
+++ EntornoPeliculas/modelsPydantic.py
@@ -5,4 +5,4 @@ class Pelicula(BaseModel):
     titulo: str = Field(min_length=2)
     genero: str = Field(min_length=4)
     anio: int = Field(ge=1000, le=9999) #no jala si le pongo aÑo jasjaja
-    clasificacion: Literal["A", "B", "C"]
+    clasificacion: Literal["A", "B", "C"] 
